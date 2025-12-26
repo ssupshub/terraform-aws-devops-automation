eda0058f00b1b9e35208b0a5cfbf094fe48fe02a
Shubham <140893209+ssupshub@users.noreply.github.com>
2025-12-26 23:34:27 +0530

    Terraform state management

---
+++ Remote Backend with S3 & DynamoDB/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket         = "terraform-state-bucket"
+    key            = "devops/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-lock"
+    encrypt        = true
+  }
+}
