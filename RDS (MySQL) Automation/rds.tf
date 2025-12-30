76e8b06b760bf4432d7258dfddf4c8dff6053c0f
Shubham <140893209+ssupshub@users.noreply.github.com>
2025-12-30 20:31:03 +0530

    Create rds.tf

---
+++ RDS (MySQL) Automation/rds.tf
@@ -0,0 +1,10 @@
+resource "aws_db_instance" "mysql" {
+  allocated_storage    = 20
+  engine               = "mysql"
+  engine_version       = "8.0"
+  instance_class       = "db.t3.micro"
+  name                 = var.db_name
+  username             = "admin"
+  password             = "password123"
+  skip_final_snapshot  = true
+}
